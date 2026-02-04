00df909450aa8d8dccf3e94b3cafed75998589f9
Aleksandr Kukolev <akukolev@gehtsoft.com>
2026-02-04 19:36:08 +0100

    Digit cells with 0 made clear.

--- src/components/MainForm.vue
+++ src/components/MainForm.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="header">
     <h1 class="main-form-title">{{ message }}</h1>
-    <button @click="reload()">Загрузить новый</button>
-    <button @click="reload()">Проверить</button>
+    <button name="nonogram-reload-button" @click="requestReload()">Загрузить новый</button>
+    <button name="nonogram-check-button" @click="reload()">Проверить</button>
   </div>
   <div class="main-form">
     <Nonogram :key="componentKey" :size="nonogramSize" :row-values="rowValues" :col-values="colValues"/>
   </div>
+  <ConfirmationDialog v-if="showDialog" @yes="handleConfirm" @no="handleCancel" />
 </template>
 <style scoped>
 h1 {
@@ -25,10 +26,12 @@ defineProps({
 })
 
 import Nonogram from './Nonogram.vue';
+import ConfirmationDialog from './ConfirmationDialog.vue';
 import {ref} from 'vue';
 import {loadData, loadRandomNonogram} from '../funcs.js';
 
 const componentKey = ref(0);
+const showDialog = ref(false);
 
 
 let [rowValues, colValues] = loadRandomNonogram();
@@ -38,6 +41,19 @@ async function reload() {
   componentKey.value += 1;
   [rowValues, colValues] = loadRandomNonogram();
   nonogramSize = {rows: rowValues.length, cols: colValues.length};
+}
+
+function requestReload() {
+  showDialog.value = true;
+}
+
+function handleConfirm() {
+  showDialog.value = false;
+  reload();
+}
+
+function handleCancel() {
+  showDialog.value = false;
 }
 </script>
 
