01073a0557e0d7a274c5c7f176c9e7ac0794161d
Aleksandr Kukolev <akukolev@gehtsoft.com>
2026-02-11 22:48:14 +0100

    Buttons without logic added to admin page.

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,3 +1,18 @@
 <template>
-  This is admin page
+  <div class="admin-controls">
+    <button>Load</button>
+    <input type="text" placeholder="ID for load" />
+    <button>Good</button>
+    <button>Bad</button>
+  </div>
 </template>
+
+<style scoped>
+.admin-controls {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+}
+</style>
